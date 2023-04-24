a2943f549126ecf28b56e986870a06c2b650a1f2
“Yulia <julie.kovalchuk@gmail.com>
2023-04-24 11:14:18 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const math = require('./utils/math');
+
+console.log(math);
+
+/*
+
+resolving -> loading -> wrapping -> evalution -> caching
+
+core
+
+*/
---
+++ utils/math.js
@@ -0,0 +1,12 @@
+module.exports.test = 12;
+module.exports.obj = {
+    prop1: 1,
+    prop2: 'qwerty'
+}
+
+module.exports.f1 = () => 'f1'
+
+const f2 = () => 'f2'
+const arr = [1,2,3]
+
+console.log(module);
